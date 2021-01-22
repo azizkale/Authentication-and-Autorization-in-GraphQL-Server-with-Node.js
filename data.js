fc8e4dcea8821c9760fdade55e550b42f11d9a7c
azizkale <azizkale@hotmail.com>
2021-01-22 14:59:14 +0100

    dataAdded

---
+++ data.js
@@ -0,0 +1,24 @@
+const Users = [
+  {
+    id: 1,
+    username: "john",
+    city: "Melbourne",
+    password: "123456",
+    role: "admin",
+  },
+  {
+    id: 2,
+    username: "samet",
+    city: "Istanbul",
+    password: "123",
+    role: "user",
+  },
+  {
+    id: 3,
+    username: "maria",
+    city: "Zagreb",
+    password: "456",
+    role: "user",
+  },
+];
+module.exports = Users;
